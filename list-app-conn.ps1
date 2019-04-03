d596d61cc09654d18ac5d8b0f9c27bae7344b9b5
ftonial <38221113+ftonial@users.noreply.github.com>
2019-04-03 10:53:40 -0300

    Update list-app-conn.ps1

--- list-app-conn.ps1
+++ list-app-conn.ps1
@@ -1,18 +1,14 @@
 <#
 MIT License
-
 Copyright (c) 2018 Clever dos Anjos
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,10 +16,10 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 #>
 
-$logfolder = "C:\ProgramData\Qlik\Sense\Log\Script"
+$logfolder = "C:\QlikShare\ArchivedLogs\vmqliksense\Script"
+$export_file = "C:\QlikShare\ArchivedLogs\list-app-conn.csv"
 [System.Collections.ArrayList]$apps_connections = @()
 foreach ($log in Get-ChildItem -Path $logfolder -Filter *.log) {
     foreach ($line in Select-String -LiteralPath $log.FullName  -Pattern "\[lib:\/\/(.*)\/" -AllMatches | %{$_.matches.groups[1].Value} | Get-Unique) {
@@ -62,4 +58,5 @@ for ($i = 0; $i -lt $apps_connections.Count; $i++) {
 }
 
 $apps_connections | Format-Table
+$apps_connections | Export-Csv -Path $export_file -Delimiter ';' -NoTypeInformation
 ## End of file
